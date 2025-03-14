53224b74b7f78612a22ac08b12220b25382e31e2
jason <pochli6262@gmail.com>
2025-03-14 09:45:09 +0800

    Initial Commit

---
+++ SwiftUI_class_HW2/SwiftUI_class_HW2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_class_HW2App.swift
+//  SwiftUI_class_HW2
+//
+//  Created by 廖柏鈞 on 2025/3/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_class_HW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
